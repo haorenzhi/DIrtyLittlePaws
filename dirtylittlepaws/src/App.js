ab5faa3b7fd80ec176a7f8e92fae8fff57f3cf08
Sebastian Perez-Delgado <46662157+SebastianPD@users.noreply.github.com>
2022-02-13 21:53:33 -0600

    added maps api functionality

--- dirtylittlepaws/src/App.js
+++ dirtylittlepaws/src/App.js
@@ -1,5 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import Handlebars from 'handlebars/dist/cjs/handlebars'
+const google = window.google;
+
+
+/**
+ * Defines an instance of the Locator+ solution, to be instantiated
+ * when the Maps library is loaded.
+ */
+ const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+ export class MapContainer extends Component {
+
+  state = {
+    showingInfoWindow: false,  // Hides or shows the InfoWindow
+    activeMarker: {},          // Shows the active marker upon click
+    selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 42.05711906051999,
+            lng: -87.67628472008052
+          }
+        }
+        
+      > 
+       <Marker
+          onClick={this.onMarkerClick}
+          name={'Kenyatta International Convention Centre'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAQoEnnmtQQ0bJ8fCvmKHPOI6VtNCXD6EY'
+})(MapContainer);
+
 
 function App() {
   return (
@@ -22,4 +98,4 @@ function App() {
   );
 }
 
-export default App;
+//export default App;
