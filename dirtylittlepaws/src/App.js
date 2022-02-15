9b4ee2513f7ce9113d94f65724ae78d5cfda8218
huaxuan250 <huaxuan250@gmail.com>
2022-02-14 20:47:11 -0600

    Styling Done

--- dirtylittlepaws/src/App.js
+++ dirtylittlepaws/src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
-import Handlebars from 'handlebars/dist/cjs/handlebars'
-import { Panel, PanelGroup } from 'rsuite';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+import Handlebars from "handlebars/dist/cjs/handlebars";
+import { Panel, PanelGroup } from "rsuite";
 const google = window.google;
-
 
 /**
  * Defines an instance of the Locator+ solution, to be instantiated
  * when the Maps library is loaded.
  */
- const mapStyles = {
-  width: '80%',
-  height: '80%'
+const mapStyles = {
+  width: "500px",
+  height: "500px",
 };
 
 const instance = (
@@ -22,78 +21,70 @@ const instance = (
   </Panel>
 );
 
- export class MapContainer extends Component {
-
+export class MapContainer extends Component {
   state = {
-    showingInfoWindow: false,  // Hides or shows the InfoWindow
-    activeMarker: {},          // Shows the active marker upon click
-    selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+    showingInfoWindow: false, // Hides or shows the InfoWindow
+    activeMarker: {}, // Shows the active marker upon click
+    selectedPlace: {}, // Shows the InfoWindow to the selected place upon a marker
   };
 
   onMarkerClick = (props, marker, e) =>
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
-      showingInfoWindow: true
+      showingInfoWindow: true,
     });
 
-  onClose = props => {
+  onClose = (props) => {
     if (this.state.showingInfoWindow) {
       this.setState({
         showingInfoWindow: false,
-        activeMarker: null
+        activeMarker: null,
       });
     }
   };
 
-
   render() {
     return (
-     <div className="Panel-wrapper">
-       <Panel header="Panel title">
-         <p>HELLO WORLD</p>
+      <div className="panel_map">
+        <Panel header="Panel title" className="panel">
+          <p>I love C</p>
         </Panel>
-      
-      <div className="Map-wrapper">
-      <Map
-      
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={
-          {
-            lat: 42.05711906051999,
-            lng: -87.67628472008052
-          }
-        }
-        
-      > 
-       <Marker
-          onClick={this.onMarkerClick}
-          name={'Ford Design Institute'}
-        />
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
-        >
-          <div>
-            <h4>{this.state.selectedPlace.name}</h4>
-          </div>
-        </InfoWindow>
-      </Map>
-      </div>
-      </div>
 
-      
-      );
+        {/* Issue with Map, styling has to be local here */}
+        <div className="map">
+          <Map
+            google={this.props.google}
+            zoom={14}
+            style={mapStyles}
+            initialCenter={{
+              lat: 42.05711906051999,
+              lng: -87.67628472008052,
+            }}
+          >
+            <Marker
+              onClick={this.onMarkerClick}
+              name={"Ford Design Institute"}
+            />
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+              onClose={this.onClose}
+            >
+              <div>
+                <h4>{this.state.selectedPlace.name}</h4>
+              </div>
+            </InfoWindow>
+          </Map>
+        </div>
+      </div>
+    );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAQoEnnmtQQ0bJ8fCvmKHPOI6VtNCXD6EY'
+  apiKey: "AIzaSyAQoEnnmtQQ0bJ8fCvmKHPOI6VtNCXD6EY",
 })(MapContainer);
-
 
 function App() {
   return (
